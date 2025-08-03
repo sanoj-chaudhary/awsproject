da968631c9a97006c456509f19555513674bd0f3
Sanoj Chaudhary <101033913+sanoj-chaudhary@users.noreply.github.com>
2025-08-03 13:12:49 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 console.log(process.env.DB_HOST,"sanoj")
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+const connection = mysql.createConnection({
+  host: 'database-1.c3omya2ui7ke.eu-north-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'sanoj9900',
+  database: 'crudProject',
+  port: 3306
 });
 
 db.connect(err => {
