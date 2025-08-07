2de1f23c9711f91be57641cdeed28d3a46a6c34a
sanojChaudhary <schaudhary@solifetec.com>
2025-08-07 11:53:17 +0530

    update db connection

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 const db = mysql.createConnection({
-  host: '',
-  user: '',
-  password: '',
-  database: '',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER ||  'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'orderdb',
   port: 3306
 });
 
