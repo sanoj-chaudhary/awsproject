a4c7ecce08e839d451f01b22964b18d0982648d8
sanojChaudhary <schaudhary@solifetec.com>
2025-08-07 11:53:17 +0530

    update db connection

--- order-service/config/db.js
+++ order-service/config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_HOST, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize('orderdb', 'root', '', {
+  host: 'localhost',
   dialect: 'mysql',
 });
 
