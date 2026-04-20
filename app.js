281eba3f6f0169788bd38ac6fcf4de73eb223ec1
Schaudhary <SChaudhary@solifetec.com>
2026-04-20 14:13:45 +0530

    test

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Chat API  Gateway!');
+  res.send('Welcome to the Chat API  Gateway! Use /ask to interact with the AI.');
 });
 const OpenAI = require("openai");
 
